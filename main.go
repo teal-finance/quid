5b04d1a714706b516de7c7f87587eabf3bc57181
synw <synw@yahoo.com>
2020-07-22 22:32:03 +0200

    [Backend] add an admin endpoint to change token max ttl for a namespace

--- main.go
+++ main.go
@@ -114,6 +114,7 @@ func main() {
 	ns.POST("/find", api.FindNamespace)
 	ns.POST("/info", api.NamespaceInfo)
 	ns.POST("/key", api.GetNamespaceKey)
+	ns.POST("/maxttl", api.SetNamespaceTTL)
 	ns.POST("/endpoint", api.SetNamespaceEndpointAvailability)
 	ns.GET("/all", func(c echo.Context) error {
 		data, err := db.SelectAllNamespaces()

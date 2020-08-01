8fcb45aee819a68ab9d473d36397ae2d99e8550f
synw <synw@yahoo.com>
2020-08-01 13:04:21 +0200

    Cleanup old files

--- quidui/dist/js/chunk-2d21a3d2.23206ac6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},c=[],l=t("2877"),a={},r=Object(l["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d21a3d2.23206ac6.js.map

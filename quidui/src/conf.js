29c02bc76cf504770b88c513227851457c8b7e94
synw <synw@yahoo.com>
2020-08-07 21:30:14 +0200

    [Frontend] Fix default quidUrl conf param for production

--- quidui/src/conf.js
+++ quidui/src/conf.js
@@ -1,5 +1,8 @@
 function getQuidUrl() {
-    let url = "http://localhost:8082";
+    let url = "";
+    if (process.env.NODE !== 'production') {
+        url = "http://localhost:8082";
+    }
     if (process.env.VUE_APP_SERVER_URL !== undefined) {
         return process.env.VUE_APP_SERVER_URL;
     }

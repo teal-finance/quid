16838433f2d76ba3c306e3e405d4d2b8e9249375
synw <synwx@protonmail.com>
2022-03-12 13:37:30 +0100

    Minor fix in old ui and rebuild

--- quidui/dist/js/chunk-2d21645d.3d279a49.js
+++ quidui/dist/js/chunk-2d21645d.3d279a49.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21645d"],{c280:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("h1",{staticClass:"text-muted mt-3"},[t._v("Tokens")])},c=[],u=n("2877"),l={},a=Object(u["a"])(l,s,c,!1,null,null,null);e["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d21645d.df6ef262.js.map
+//# sourceMappingURL=chunk-2d21645d.3d279a49.js.map

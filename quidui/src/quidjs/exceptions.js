037399f6cd1868bc625960ebf56fdb63fe6a189b
synw <synwx@protonmail.com>
2022-04-08 13:40:00 +0200

    Remove old frontend

--- quidui/src/quidjs/exceptions.js
+++
@@ -1,7 +0,0 @@
-export default function quidException({ error, hasToLogin = false, unauthorized = false }) {
-    return {
-        hasToLogin: hasToLogin,
-        error: error,
-        unauthorized: unauthorized
-    }
-}

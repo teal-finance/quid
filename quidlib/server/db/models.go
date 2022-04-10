4753642a3fd67f0cd8c290cb6a2d46589fb2f814
Oliver <0uep@hessling.fr>
2022-04-10 02:39:08 +0200

    Remove dead code

--- quidlib/server/db/models.go
+++ quidlib/server/db/models.go
@@ -13,27 +13,12 @@ type user struct {
 	IsDisabled  bool      `db:"is_disabled" json:"is_disabled"`
 }
 
-type group struct {
-	ID          int64     `db:"id" json:"id"`
-	Name        string    `db:"name" json:"name"`
-	Namespace   string    `db:"namespace" json:"namespace"`
-	DateCreated time.Time `db:"date_created" json:"date_created"`
-}
-
 type userGroupName struct {
 	Name string `db:"name" json:"name"`
 }
 
 type userOrgName struct {
 	Name string `db:"name" json:"name"`
-}
-
-type token struct {
-	ID                  int32  `db:"id" json:"id"`
-	Value               string `db:"value" json:"value"`
-	UserID              int32  `db:"user_id" json:"user_id"`
-	ExpirationTimestamp int32  `db:"expiration_timestamp" json:"expiration_timestamp"`
-	Namespace           string `db:"namespace" json:"namespace"`
 }
 
 type namespace struct {

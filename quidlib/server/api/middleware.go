f8673bcd5f56b86d8f2a4e5ef96fa848e3661a69
Oliver <0uep@hessling.fr>
2022-04-13 05:08:18 +0200

    Log conversion error

--- quidlib/server/api/middleware.go
+++ quidlib/server/api/middleware.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -15,8 +16,24 @@ import (
 func AdminMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		// check the access token to control that the user is admin
-		u := c.Get("user").(*jwt.Token)
-		claims := u.Claims.(*tokens.AccessClaims)
+		u, ok := c.Get("user").(*jwt.Token)
+		if !ok {
+			emo.Error("Wrong JWT type in context user: ", c.Get("user"))
+			log.Panicf("Wrong JWT type in context user: %T %v", c.Get("user"), c.Get("user"))
+			return c.JSON(http.StatusConflict, echo.Map{
+				"error": "Wrong JWT type in context user",
+			})
+		}
+
+		claims, ok := u.Claims.(*tokens.AccessClaims)
+		if !ok {
+			emo.Error("Wrong AccessClaims type for claims: ", u.Claims)
+			log.Panic("Wrong AccessClaims type for claims: ", u.Claims)
+			return c.JSON(http.StatusConflict, echo.Map{
+				"error": "Wrong AccessClaims type for claims",
+			})
+		}
+
 		isAdmin := false
 		for _, g := range claims.Groups {
 			if g == "quid_admin" {

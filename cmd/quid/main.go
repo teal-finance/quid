df7b6bbf73730be0114bb44078ac20356dbf49f8
Oliver <0uep@hessling.fr>
2022-09-14 22:31:21 +0200

    🩹 Add workaround for Heroku-generated secret of 64 hex digits

--- cmd/quid/main.go
+++ cmd/quid/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"flag"
+	"net/url"
 
 	"github.com/teal-finance/emo"
 	"github.com/teal-finance/garcon/gg"
 	"github.com/teal-finance/quid/crypt"
 	"github.com/teal-finance/quid/server/api"
 	"github.com/teal-finance/quid/server/db"
-	"github.com/teal-finance/quid/tokens"
 )
 
 var log = emo.NewZone("quid")
 
 const (
 	// defaultKey is AES-128-bits (16 bytes) in hexadecimal form (32 digits).
-	defaultKey  = "00112233445566778899aabbccddeeff"
-	defaultUsr  = "admin"
-	defaultPwd  = "my_password"
-	defaultPort = 8090
-
+	// Attention: Heroku generates secrets with 64 hexadecimal digits.
+	defaultKey    = "00112233445566778899aabbccddeeff"
+	defaultUsr    = "admin"
+	defaultPwd    = "my_password"
 	defaultDBUser = "pguser"
 	defaultDBPass = "my_password"
 	defaultDBHost = "localhost"
@@ -48,7 +47,7 @@ func main() {
 		dbName  = flag.String("db-name", gg.EnvStr("DB_NAME", defaultDBName), "Name of the Postgres database. Env. var: DB_NAME")
 		dbURL   = flag.String("db-url", gg.EnvStr("DB_URL", defaultDBurl), "The endpoint of the PostgreSQL server. Env. var: DB_URL")
 		www     = flag.String("www", gg.EnvStr("WWW_DIR", "ui/dist"), "Folder of the web static files. Env. var: WWW_DIR")
-		port    = flag.Int("port", gg.EnvInt("PORT", defaultPort), "Listening port of the Quid server")
+		port    = flag.Int("port", gg.EnvInt("PORT", 8090), "Listening port of the Quid server")
 	)
 	flag.Parse()
 
@@ -72,29 +71,36 @@ func main() {
 
 	if (!*dev) && (*key == defaultKey) && (*dbURL == defaultDBurl) {
 		if !*verbose {
-			log.Print("Default values for -dev, -key QUID_KEY and -db DB_URL => Enable -v verbose mode")
+			log.Print("Enable -v verbose mode because default values for -dev, -key QUID_KEY and -db DB_URL")
 			*verbose = true
 		}
-		log.Print("Default values for -key QUID_KEY and -db DB_URL => Enable -dev mode")
+		log.Print("Enable -dev mode because default values for -key QUID_KEY and -db DB_URL")
 		*dev = true
 	}
 	emo.GlobalVerbosity(*verbose)
 
-	log.Param("-dev                  =", *dev)
-	log.Param("-v                    =", *verbose)
-	log.Param("-conf                 =", *conf)
-	log.Param("-key   QUID_KEY       =", *key)
-	log.Param("-admin QUID_ADMIN_USR =", *admin)
-	log.Param("-pwd   QUID_ADMIN_PWD =", *pwd)
-	log.Param("-conf                 =", *conf)
-	log.Param("-db-usr  DB_USR       =", *dbUser)
-	log.Param("-db-pwd  DB_PWD       =", *dbPass)
-	log.Param("-db-host DB_HOST      =", *dbHost)
-	log.Param("-db-port DB_PORT      =", *dbPort)
-	log.Param("-db-name DB_NAME      =", *dbName)
-	log.Param("-db-url  DB_URL =", *dbURL)
-	log.Param("-www     WWW_DIR      =", *www)
-	log.Param("-port    PORT         =", *port)
+	obfuscatedPwdURL := *dbURL
+	u, err := url.Parse(*dbURL)
+	if err == nil {
+		*dbURL = u.String()
+		obfuscatedPwdURL = u.Redacted()
+	}
+
+	log.V().Param("-dev                  =", *dev)
+	log.V().Param("-v                    =", *verbose)
+	log.V().Param("-conf                 =", *conf)
+	log.V().Param("-key   QUID_KEY  size =", len(*key), "bytes")
+	log.V().Param("-admin QUID_ADMIN_USR =", *admin)
+	log.V().Param("-pwd   QUID_ADMIN_PWD =", len(*pwd), "bytes")
+	log.V().Param("-conf                 =", *conf)
+	log.V().Param("-db-usr  DB_USR       =", *dbUser)
+	log.V().Param("-db-pwd  DB_PWD  size =", len(*dbPass), "bytes")
+	log.V().Param("-db-host DB_HOST      =", *dbHost)
+	log.V().Param("-db-port DB_PORT      =", *dbPort)
+	log.V().Param("-db-name DB_NAME      =", *dbName)
+	log.V().Param("-db-url  DB_URL       =", obfuscatedPwdURL)
+	log.V().Param("-www     WWW_DIR      =", *www)
+	log.V().Param("-port    PORT         =", *port)
 
 	if *conf {
 		log.Info(`Generating "config.json" file with random AES-128 key`)
@@ -105,9 +111,15 @@ func main() {
 		return
 	}
 
-	crypt.EncodingKey = tokens.DecodeHexOrB64(*key, 16)
-	if crypt.EncodingKey == nil {
-		log.Panic("Want AES-128 key in hexadecimal (32 digits) or Base64 (unpadded 22 characters RFC 4648 §5), but got", len(*key), "bytes:", *key)
+	// fix for Heroku-generated secret always composed of 64 hexadecimal digits
+	if len(*key) == 64 {
+		*key = (*key)[:32]
+	}
+
+	crypt.EncodingKey, err = gg.DecodeHexOrB64(*key, 16)
+	if err != nil {
+		log.Error(err)
+		log.Fatal("Want AES-128 key in hexadecimal (32 digits) or Base64 (unpadded 22 characters, RFC 4648 §5), but got", len(*key), "bytes:", *key)
 	}
 
 	if err := db.Connect(*dbURL); err != nil {
@@ -118,8 +130,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err := db.CreateQuidAdmin(*admin, *pwd)
-	if err != nil {
+	if err := db.CreateQuidAdmin(*admin, *pwd); err != nil {
 		log.Fatal(err)
 	}
 

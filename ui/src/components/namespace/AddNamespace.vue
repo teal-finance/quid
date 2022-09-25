cdfc7815a9bc4810c9e272b34c9e9f2429747c6f
synw <synwx@protonmail.com>
2022-09-25 13:18:29 +0200

    Add algo support in namespace creation

--- ui/src/components/namespace/AddNamespace.vue
+++ ui/src/components/namespace/AddNamespace.vue
@@ -8,6 +8,25 @@
       :validator="form.refreshTokenTtl.validator" inline-label="Refresh tokens max time to live" required></sw-input>
     <sw-switch id="ns-switch" class="switch-success" v-model:value="enablePublicEndpoint">&nbsp;Enable public endpoint
     </sw-switch>
+    <div class="flex flex-row items-center">
+      <div class="mr-2">Algorithm:</div>
+      <select v-model="algo">
+        <option>HS256</option>
+        <option>HS384</option>
+        <option>HS512</option>
+        <option>RS256</option>
+        <option>RS384</option>
+        <option>RS512</option>
+        <option>PS256</option>
+        <option>PS384</option>
+        <option>PS512</option>
+        <option>ES256</option>
+        <option>ES384</option>
+        <option>ES512</option>
+        <option>EDDSA</option>
+      </select>
+    </div>
+
     <div class="flex flex-row">
       <button class="w-20 mr-3 btn success" :disabled="!isFormValid === true" @click="postForm()">Save</button>
       <button class="w-20 btn warning" @click="onCancel()">Cancel</button>
@@ -21,10 +40,12 @@ import SwInput from "@snowind/input";
 import SwSwitch from "@snowind/switch";
 import { api } from "@/api";
 import { notify } from "@/state";
+import { AlgoType } from "@/interface";
 
 const emit = defineEmits(["end"]);
 
 const enablePublicEndpoint = ref(false);
+const algo = ref<AlgoType>("HS256");
 const form = reactive({
   name: {
     val: "",
@@ -64,6 +85,7 @@ async function postForm() {
   try {
     await api.post("/admin/namespaces/add", {
       name: form.name.val,
+      alg: algo.value,
       max_ttl: form.accessTokenTtl.val,
       refresh_max_ttl: form.refreshTokenTtl.val,
       enable_endpoint: enablePublicEndpoint.value,
